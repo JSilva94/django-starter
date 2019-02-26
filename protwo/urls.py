5276d7e1f0eb75ddcebd1c1558fbd9d01d8b9398
Jayde Silva <jayde.silva@mail.utoronto.ca>
2019-02-26 00:47:29 -0500

    added debug toolbar

--- protwo/urls.py
+++ protwo/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from apptwo import views
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('users/', include('apptwo.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls))
+    ] + urlpatterns
